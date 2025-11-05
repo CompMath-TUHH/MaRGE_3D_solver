5864ce48c6b53974b3481aba045900ca574d69d7
Vamika Rathi <vamika.rathi@tuhh.de>
2025-11-05 10:01:39 +0100

    Numerical solver for MaRGE in 3D flow

---
+++ Dtche_J_parameters_3D.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  9 12:23:02 2024
+
+@author: cvr6133
+
+This is the list of parameters required for 3D MRE as solved according to Daitche's paper
+"""
+
+class mr_parameter(object):
+
+  def set_beta(self):
+    self.beta = self.rho_p /self.rho_f
+  
+  def set_S(self):
+    self.S = (1.0/3.0)*self.a**2/(self.nu * self.T)
+  
+  def set_R(self):
+    self.R = 3.0/(1.0 + 2.0*self.beta)
+    
+  def set_gravity(self):
+    self.g = (self.T/self.U)*9.8
+  
+  def __init__(self, particle_density, fluid_density, particle_radius,
+                     kinematic_viscosity, time_scale, char_vel):
+    self.rho_p = particle_density
+    self.rho_f = fluid_density
+    self.a     = particle_radius
+    self.nu    = kinematic_viscosity
+    self.T     = time_scale
+    self.U     = char_vel
+
+    self.set_beta()
+    self.set_S()
+    self.set_R()
+    self.set_gravity()
